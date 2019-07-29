ec58681fc4279dca54dbd637cf28302e3143f240
iamtheluiz <iamtheluiz.dev@gmail.com>
2019-07-28 21:08:48 -0300

    Add basic routes and not found page

---
+++ src/pages/NotFound/index.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import ReactLogo from '../../logo.svg';
+
+// Page style
+import './index.css';
+
+export default class App extends Component{
+  // Before component render
+  componentDidMount(){
+    // Set page title
+    document.title = "Not Found!"
+  }
+
+  // Display 
+  render(){
+    return (
+      <div className="App">
+        <img src={ReactLogo} alt="React logo"></img>
+        <h1>Error 404</h1>
+        <p>This is not a page from the site!</p>
+        <a href="/">Go back to Home</a>
+      </div>
+    );
+  }
+}
